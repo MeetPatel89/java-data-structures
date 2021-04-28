3d2e659dff2a2420b5700af78f5ffb7dfad1781d
Meet Patel <meetaiims@yahoo.com>
2021-04-28 10:22:18 -0500

    add class Node to instantiate node objects with private fields- data and next

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello Main");
+		Node nodeObj1 = new Node("Node One");
+		Node nodeObj2 = new Node("Node Two");
+		nodeObj1.setNextNode(nodeObj2);
+		System.out.println(nodeObj1.getNextNode());
+		System.out.println(nodeObj2);
+		System.out.println(nodeObj1.getNextNode().getData());
+	}
+}
--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,26 @@
 
 public class Node {
-
+	private String data;
+	private Node next;
+	
+	public Node(String data) {
+		this.data = data;
+	}
+	
+	public String getData() {
+		return this.data;
+	}
+	
+	public Node getNextNode() {
+		return this.next;
+	}	
+	
+	public void setNextNode(Node node) {
+		this.next = node;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Node");
+		
+	}
 }
